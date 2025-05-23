dba5a02e90efdec06e40ae45a243ea951b1b83d4
na-kthr <a510.md14@gmail.com>
2025-05-23 14:44:44 +0900

    コンフリクト修正

--- testGit/src/testGit/HelloGit.java
+++ testGit/src/testGit/HelloGit.java
@@ -3,11 +3,8 @@ package testGit;
 public class HelloGit {
 	public static void main(String[] args) {
 		System.out.println("Hello GitHub!");
-<<<<<<< HEAD
 		System.out.println("GoodBye GitHub!");
-=======
 		System.out.println("GoodMorning GitHub!");
->>>>>>> branch 'main' of https://github.com/na-kthr/TestEclips.git
 	}
 
 }
